bf4176fbf4ec788830789d42e789ffad3a8f82a0
Myles Braithwaite <myles@myles.garden>
2026-06-12 20:58:22 -0400

    chore: adds the SITE_URL consts

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: Update this with the URL of the website.
-  site: "https://localhost:4321",
+  site: SITE_URL,
   integrations: [
     sitemap({
       serialize(item) {
