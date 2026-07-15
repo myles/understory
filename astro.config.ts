a5c18acbe68c2388d6067e96c639c522a4252f5c
myles <myles@myles.garden>
2026-07-15 14:15:15 -0400

    chore(docs): don't need that code comment

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: Update this with the URL of the website.
   site: SITE_URL,
   vite: {
     plugins: [tailwindcss()],
